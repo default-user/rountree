c678de344f0a0b33edbf9a515c59b92f08e14307
Ande TURNER <ande.turner@gmail.com>
2013-10-06 20:59:54 +1300

    Update flexarray.h

--- flexarray.h
+++ flexarray.h
@@ -6,8 +6,11 @@ typedef struct flexarray_record *flexarray;
 extern flexarray flexarray_new();
 
 extern void flexarray_append(flexarray, int);
+
 extern void flexarray_free(flexarray);
+
 extern void flexarray_print(flexarray);
+
 extern void flexarray_sort(flexarray);
 
 #endif
