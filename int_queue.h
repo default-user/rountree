724b4dddc67dced44057eb489d3aff49bbe64760
Ande TURNER <ande.turner@gmail.com>
2013-10-06 21:00:51 +1300

    Create int_queue.h

---
+++ int_queue.h
@@ -0,0 +1,24 @@
+/**
+   int_queue.h
+   @author TURNER. Ande,
+   Defines the usage of the Queue ADT.
+**/
+
+#ifndef INT_QUEUE_H_
+#define INT_QUEUE_H_
+
+typedef struct int_queue_record *int_queue;
+
+extern int int_queue_size(int_queue);
+
+extern int int_dequeue(int_queue);
+
+extern int_queue int_queue_new();
+
+extern void int_enqueue(int_queue, int);
+
+extern void int_queue_free(int_queue);
+
+extern void int_queue_print(int_queue);
+
+#endif
