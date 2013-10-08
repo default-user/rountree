33528631d9bc05d526accd124ee07a32c034a7e0
Ande TURNER <ande.turner@gmail.com>
2013-10-08 21:32:18 +1300

    Create mysorts.h

---
+++ mysorts.h
@@ -0,0 +1,12 @@
+#ifndef MYSORTS_H_
+#define MYSORTS_H_
+
+extern void insertion_sort(int *, int);
+
+extern void merge_sort(int *, int);
+
+extern void quick_sort(int *, int);
+
+extern void selection_sort(int *, int);
+
+#endif
