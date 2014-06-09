9405bc5252d94ff76df4b28d6139fea168cff933
Ande TURNER <ande.turner@gmail.com>
2014-06-10 07:40:22 +1200

    Update mylib.c

--- mylib.c
+++ mylib.c
@@ -1,3 +1,9 @@
+/**
+   mylib.c
+   @author TURNER. Ande,
+   Implements "my library".
+**/
+
 #include <stdio.h> /* for fprintf */
 #include <stdlib.h> /* for size_t, malloc, realloc, exit */
 #include <assert.h>
@@ -7,23 +13,19 @@
 
 void *emalloc(size_t memory_requirement) {
    void *memory_allocation = malloc(memory_requirement);
-   
    if (NULL == memory_allocation) {
       fprintf(stderr, "!!! MEMORY ALLOCATION FAILURE !!!");
       exit(EXIT_FAILURE);
    }
-   
    return memory_allocation;
 }
 
 void *erealloc(void *memory_allocation, size_t memory_requirement) {
    void *memory_reallocation = realloc(memory_allocation, memory_requirement);
-   
    if (NULL == memory_reallocation) {
       fprintf(stderr, "!!! MEMORY REALLOCATION FAILURE !!!");
       exit(EXIT_FAILURE);
    }
-   
    return memory_reallocation;
 }
 
@@ -31,18 +33,15 @@ int getword(char *string, int limit, FILE *stream) {
    int character;
    char *word = string;
    assert(limit > 0 && NULL != string && NULL != stream);
-   
    /* skip to the start of the word */
    while (!isalnum(character = getc(stream)) && EOF != character)
       ;
    if (EOF == character) {
       return EOF;
-      
    /* reduce limit by 1 to allow for the \0 */
    } else if (--limit > 0) { 
       *word++ = tolower(character);
    }
-   
    while (--limit > 0) {
       if (isalnum(character = getc(stream))) {
          *word++ = tolower(character);
@@ -52,7 +51,6 @@ int getword(char *string, int limit, FILE *stream) {
          break;
       }
    }
-   
    *word = '\0';
    return word - string;
 }
