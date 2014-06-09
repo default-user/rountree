3e0d7d0ea67cc2922d467f1c7dd97ff841aca2d2
Ande TURNER <ande.turner@gmail.com>
2014-06-10 07:25:14 +1200

    Update mylib.c

--- mylib.c
+++ mylib.c
@@ -7,26 +7,30 @@
 
 void *emalloc(size_t memory_requirement) {
    void *memory_allocation = malloc(memory_requirement);
+   
    if (NULL == memory_allocation) {
       fprintf(stderr, "!!! MEMORY ALLOCATION FAILURE !!!");
       exit(EXIT_FAILURE);
    }
+   
    return memory_allocation;
 }
 
 void *erealloc(void *memory_allocation, size_t memory_requirement) {
    void *memory_reallocation = realloc(memory_allocation, memory_requirement);
+   
    if (NULL == memory_reallocation) {
       fprintf(stderr, "!!! MEMORY REALLOCATION FAILURE !!!");
       exit(EXIT_FAILURE);
    }
+   
    return memory_reallocation;
 }
 
 int getword(char *string, int limit, FILE *stream) {
    int character;
    char *word = string;
-   assert(limit > 0 && string != NULL && stream != NULL);
+   assert(limit > 0 && NULL != string && NULL != stream);
    
    /* skip to the start of the word */
    while (!isalnum(character = getc(stream)) && EOF != character)
@@ -38,6 +42,7 @@ int getword(char *string, int limit, FILE *stream) {
    } else if (--limit > 0) { 
       *word++ = tolower(character);
    }
+   
    while (--limit > 0) {
       if (isalnum(character = getc(stream))) {
          *word++ = tolower(character);
@@ -47,6 +52,7 @@ int getword(char *string, int limit, FILE *stream) {
          break;
       }
    }
+   
    *word = '\0';
    return word - string;
 }
