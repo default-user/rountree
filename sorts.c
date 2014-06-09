3c8e5ec6a6d31855cad83887b436777bd701df59
Ande TURNER <ande.turner@gmail.com>
2014-06-10 07:56:29 +1200

    Update sorts.c

--- sorts.c
+++ sorts.c
@@ -1,149 +1,121 @@
+/**
+   sorts.c
+   @author TURNER. Ande,
+   @comments ROUNTREE et al.
+   Implements a Selection of Sorting Algorithms.
+**/   
+
 #include "mylib.h"
 #include "sorts.h"
 
 static void mergesort_merge(int *array, int *workspace, int length) {
-
    /* initialise indices to point to the beginning of */
    /* the left and right halves of array */
-   int position = 0;
+   int i = 0;
    int left_i = 0;
    int right_i = length / 2;
-   
    /* while there are elements in both halves of array { */
    while (left_i < (length / 2) && right_i < length) {
-   
       /* compare the elements at the current left and right indices */
       if (array[left_i] < array[right_i]) {   
-      
          /* put the smallest into workspace and increment both the index */
          /* it was taken from, and the index in workspace */
-         workspace[position++] = array[left_i++];
+         workspace[i++] = array[left_i++];
       } else {
-         workspace[position++] = array[right_i++];
+         workspace[i++] = array[right_i++];
       }
    }
-   
    /* add any remaining elements from left half of array to workspace */
    while (left_i < (length / 2)) {
-      workspace[position++] = array[left_i++];
+      workspace[i++] = array[left_i++];
    }
-   
    /* add any remaining elements from right half of array to workspace */
    while (right_i < length) {
-      workspace[position++] = array[right_i++];
+      workspace[i++] = array[right_i++];
    }
 }
 
 static void mergesort_sort(int *array, int *workspace, int elements) {
-
-   int position;
-   
+   int i;
    /* take care of stopping condition first */
    /* if the array to be sorted has fewer than two elements then return */
    if (elements < 2) return;
-   
    /* call merge_sort on the first half of the main array */
    mergesort_sort(array, workspace, elements / 2);
-   
    /* call sort on the second half of the main array */
    mergesort_sort(array + (elements / 2), workspace, elements - (elements / 2));
-   
    /* merge the two halves of the workspace array into the main array */
    mergesort_merge(array, workspace, elements);
-   
    /* copy the workspace array back into the main array */
-   for (position = 0; position < elements; position++) { 
-      array[position] = workspace[position];
+   for (i = 0; i < elements; i++) { 
+      array[i] = workspace[i];
    }
 }
 
 static void swap_ints(int *a, int *b) {
-	
    int temp = *a;
-   
    *a = *b;
    *b = temp;
 }
 
 void insertion_sort(int *array, int elements) {
-	
    int greater_i;
    int key;
-   int position;
-   
+   int i;
    /* for each position in array a except the first */
-   for (position = 1; position < elements; position++) {
-   
+   for (i = 1; i < elements; i++) {
       /* pull out the item at that position and store it in variable 'key' */
-      key = array[position];
-      
+      key = array[i];
       /* move each item that is to the left of that position,
          and is greater than key, one place to the right */
-      greater_i = position - 1;
-      
+      greater_i = i - 1;
       while (greater_i >= 0 && array[greater_i] > key) {
          array[greater_i + 1] = array[greater_i];
          greater_i--;
       }
-      
       /* put key in the leftmost position */
       array[greater_i + 1] = key;
    }
 }
 
 void merge_sort(int *array, int elements) {
-	
    void *workspace = emalloc(sizeof array[0] * elements);
-   
    mergessort_sort(array, workspace, elements);
-   
    free(workspace);
 }
 
 void quick_sort(int *array, int elements) {
-	
    int left_i;
    int pivot;
    int right_i;
-   
    /* if there are less than two items in the array then stop */
    if (elements < 2) return;
-   
    /* let pivot hold a copy of the array’s first element */
    pivot = array[0];
-   
    /* let left_i be an index one to the left of the array’s left-most position */
    left_i = -1;
-   
    /* let right_i be an index one to the right of the array’s right-most position */
    right_i = elements;
-   
    /* loop forever { */
    for (;;) {
-   
       /*  increment left_i (at least once) while the value at position i < pivot */
       do {
          left_i++;
       } while (array[left_i] < pivot); 
-      
       /*  decrement right_i (at least once) while the value at position j > pivot */
       do {
          right_i--;
       } while (array[right_i] > pivot);
-      
       /*  if left_i is to the left of right_i, then swap the values at their positions */
       if (left_i < right_i) {
          swap_ints(&a[left_i], &a[right_i]);
-         
          /*  else break out of the loop */
       } else {
          break;
       }
    }
-   
    /* quicksort the left sub-array */
    quick_sort(array, right_i + 1);
-   
    /* quicksort the right sub-array */
    quick_sort(array + right_i + 1, elements - right_i - 1);
 }
@@ -152,10 +124,8 @@ void selection_sort(int *array, int elements) {
    int inner_i;
    int outer_i; 
    int smallest_position;
-   
    /* for each position outer_i in the array a except the last one */
    for (outer_i = 0; outer_i < elements - 1; outer_i++) {
-   
       /* find the smallest item from position outer_i to position (n - 1) */
       smallest_position = i;
       for (inner_i = outer_i + 1; inner_i < elements; inner_i++) {
@@ -166,4 +136,3 @@ void selection_sort(int *array, int elements) {
       swap_ints(&array[outer_i], &array[smallest_position]);
    }
 }
-
